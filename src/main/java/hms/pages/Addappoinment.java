b1c387cfccdd181a2e3b9b8d2d1ab8b1ad8faded
paavarasu <paavarasu03@gmail.com>
2022-08-07 01:22:43 +0530

    Add appoinment script

---
+++ src/main/java/hms/pages/Addappoinment.java
@@ -0,0 +1,13 @@
+package hms.pages;
+
+import org.openqa.selenium.support.PageFactory;
+
+import hms.testbase.Testbase;
+
+public class Addappoinment extends Testbase {
+
+	public Addappoinment()
+	{
+		PageFactory.initElements(driver, actvit);
+	}
+}
